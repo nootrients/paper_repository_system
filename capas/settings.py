de978b2938b72a1770e4e241e9ac1f2f3d2fba32
Brian Villanueva <bclvillanueva.college@gmail.com>
2024-06-19 14:16:01 +0800

    Setup Pytest for Django

--- capas/settings.py
+++ capas/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # External Packages
+    "rest_framework",
+    # Internal Packages
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {}
