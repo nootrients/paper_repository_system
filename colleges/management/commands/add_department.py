c51ae95d05d60a4f59a44717e265556edcc85333
Brian Villanueva <bclvillanueva.college@gmail.com>
2024-06-21 18:17:25 +0800

    Finished add_department.py

--- colleges/management/commands/add_department.py
+++ colleges/management/commands/add_department.py
@@ -6,6 +6,30 @@ class Command(BaseCommand):
     help = "Inserts a list of values or instances for `Department` table."
 
     def handle(self, *args, **options):
-        Department.objects.create(department_name="Computer Science")
+        Department.objects.create(department_name="College of Accountancy and Finance")
+        Department.objects.create(department_name="College of Arts and Letters")
+        Department.objects.create(
+            department_name="College of Architecture, Design and the Built Environment"
+        )
+        Department.objects.create(department_name="College of Business Administration")
+        Department.objects.create(
+            department_name="College of Computer and Information Sciences"
+        )
+        Department.objects.create(department_name="College of Communication")
+        Department.objects.create(department_name="College of Education")
+        Department.objects.create(department_name="College of Engineering")
+        Department.objects.create(department_name="College of Human Kinetics")
+        Department.objects.create(
+            department_name="College of Political Science and Public Administration"
+        )
+        Department.objects.create(department_name="College of Science")
+        Department.objects.create(
+            department_name="College of Social Sciences and Development"
+        )
+        Department.objects.create(
+            department_name="College of Tourism, Hospitality and Transportation Management"
+        )
+        Department.objects.create(department_name="Open University System")
+        Department.objects.create(department_name="Institute of Technology")
 
         self.stdout.write(self.style.SUCCESS("Successfully added Department records"))
